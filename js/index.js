67ad393f1a5c7a5d7165d192b7824bebf006cf87
matt-shaw-1526 <122370092+matt-shaw-1526@users.noreply.github.com>
2023-01-17 09:49:44 -0500

    updating index.js with changes

--- js/index.js
+++ js/index.js
@@ -7,6 +7,21 @@ output += 'let b = 5;\n';
 output += 'let c;\n';
 output += '----------\n';
 output += ('a + b = ' + (a + b) + '\n');
+output += ('a - b = ' + (a - b) + '\n');
+output += ('a * b = ' + (a * b) + '\n');
+output += ('a / b = ' + (a / b) + '\n');
+output += ('a % b = ' + (a % b) + '\n');
+output += ('(a += b): ' + (a += b) + '\n');
+output += ('(a -= b): ' + (a -= b) + '\n');
+output += ('(a *= b): ' + (a *= b) + '\n');
+output += ('(a /= b): ' + (a /= b) + '\n');
+output += ('(a %= b): ' + (a %= b) + '\n');
+output += ('(a == b): ' + (a == b) + '\n');
+output += ('(a != b): ' + (a != b) + '\n');
+output += ('(a > b): ' + (a > b) + '\n');
+output += ('(a < b): ' + (a < b) + '\n');
+output += ('(!a && !c): ' + (!a && !c) + '\n');
+output += ('(!a || !c): ' + (!a || !c) + '\n');
 
 alert(output)
 
