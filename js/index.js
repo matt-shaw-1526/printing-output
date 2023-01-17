e2048435a9747cd0a6d04637107a61acf34ab757
matt-shaw-1526 <122370092+matt-shaw-1526@users.noreply.github.com>
2023-01-17 09:28:21 -0500

    js code added to the js file

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
+let a = 3;
+let b = 5;
+let c;
 
+let output = 'let a = 3;\n';
+output += 'let b = 5;\n';
+output += 'let c;\n';
+output += '----------\n';
+output += ('a + b = ' + (a + b) + '\n');
+
+alert(output)
+
+// let sum = a+b;
+// let sub = a-b;
+// let nul = a*b;
+
+// alert ('a+b=' + sum + '\n');
