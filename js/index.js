db2e332dcf5827ee408e0b99c76fb1ea97f8f214
matt-shaw-1526 <122370092+matt-shaw-1526@users.noreply.github.com>
2023-01-24 17:36:29 -0500

    final update for index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
+//Task 1 - Declare Variables
 let a = 3;
 let b = 5;
 let c;
 
+//Task 1 - Concatenation
 let output = 'let a = 3;\n';
 output += 'let b = 5;\n';
 output += 'let c;\n';
@@ -23,10 +25,27 @@ output += ('(a < b): ' + (a < b) + '\n');
 output += ('(!a && !c): ' + (!a && !c) + '\n');
 output += ('(!a || !c): ' + (!a || !c) + '\n');
 
+//Task 1 - Trigger alert function
 alert(output)
 
-// let sum = a+b;
-// let sub = a-b;
-// let nul = a*b;
+// Task 2 - Declare variables
+let first_name = 'Matthew';
+let last_name = 'Shaw';
+let email = 'shaw0247@algonquinlive.com';
+let sentence = output;
 
-// alert ('a+b=' + sum + '\n');
+//Task 2 - Concatenation
+sentence = 'My name is';
+sentence += ' ';
+sentence += first_name;
+sentence += ' ';
+sentence += last_name;
+sentence += '.';
+sentence += ' ';
+sentence += 'You can contact me at';
+sentence += ' ';
+sentence += email;
+sentence += '.';
+
+// Task 2 - alert output
+alert(sentence);
